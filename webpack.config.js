038e2400d26a456af79bfffbab376d1db87a5065
Adela Mag <adela_mag@yahoo.com>
2025-04-29 14:48:40 +0300

    webpack.config.js - modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict' //enable strict mode for better error handling and debugging
 
 // Import required modules
 const path = require('path') // Module to handle and transform file paths
